145c7109de9cb612a0b1bbfb73be1042bceef866
phitchcock <phitchcock@me.com>
2014-07-30 11:33:12 -0700

    added views for signup and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.user_id
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "You are signed up!"
     else
       render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit!
   end
 end
