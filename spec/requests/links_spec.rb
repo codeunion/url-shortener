a798109463107d0c3f3ee2e7fe60045fc2434a6f
Jesse Farmer <jesse@codeunion.io>
2014-07-30 14:12:25 -0700

    Add request specs for short URL redirects
    - For a valid short URL, ensure that we respond with a 302 redirect
    - For an invalid short URL, ensure that we respond with a 404

--- spec/requests/links_spec.rb
+++ spec/requests/links_spec.rb
@@ -9,4 +9,25 @@ RSpec.describe "Links", :type => :request do
       expect(response.status).to be(200)
     end
   end
+
+  describe "GET /l/:short_name" do
+    context "when short_name exists" do
+      let(:link) { FactoryGirl.create(:link) }
+
+      it "redirects to the link's URL" do
+        get link_path(link)
+
+        expect(response).to redirect_to(link.url)
+        expect(response.status).to be(302)
+      end
+    end
+
+    context "when the short_name doesn't exist" do
+      it "responds with a 404" do
+        get link_path("does-not-exist")
+
+        expect(response.status).to be(404)
+      end
+    end
+  end
 end
